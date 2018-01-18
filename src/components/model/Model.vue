6e9ed0ccb5dc705a5d51144de7fb4faaf9d17135
Colin Stevenson <colin.stevenson@ihsmarkit.com>
2018-01-17 21:42:27 -0700

    Refactors tree

--- src/components/model/Model.vue
+++ src/components/model/Model.vue
@@ -1,54 +1,63 @@
 <template>
-  <div class="card model mb-3">
-    <div class="card-body">
-      <h1>{{model.getName()}}</h1>
-      <div ref="stage">
-        <svg ref="svg">
-      	</svg>
-      </div>
-      <p class="model-branch text-success">
-        <strong>Current output: </strong>{{model.toString()}}
-      </p>
-    </div>
-  </div>
+	<div class="card model mb-3">
+		<div class="card-body">
+			<h1>{{model.getName()}}</h1>
+			<div ref="stage">
+				<svg ref="svg">
+				</svg>
+			</div>
+			<p class="model-branch text-success">
+				<strong>Current output: </strong>{{model.toString()}}
+			</p>
+		</div>
+	</div>
 </template>
 <script>
 import { mapGetters } from 'vuex'
 import Tree from '../../visualizations/tree'
 
 export default {
-  computed: {
-    ...mapGetters(['model']),
-    branch () {
-      return this.model._branch
-    }
-  },
-  methods: {
-    getStageDimensions () {
-      return [600, this.$refs.stage.offsetWidth]
-    }
-  },
-  mounted () {
-    const stageDimensions = this.getStageDimensions()
-    new Tree(this.$refs.svg, this.branch, {width: stageDimensions[1], height: stageDimensions[0]}) // eslint-disable-line no-new
-  }
+	computed: {
+		...mapGetters(['model']),
+		branch () {
+			return this.model._branch
+		}
+	},
+	methods: {
+		getStageDimensions () {
+			return [600, this.$refs.stage.offsetWidth]
+		}
+	},
+	mounted () {
+		const stageDimensions = this.getStageDimensions()
+		new Tree(this.$refs.svg, this.branch, {width: stageDimensions[1], height: stageDimensions[0]}) // eslint-disable-line no-new
+	}
 }
 </script>
-<style>
+<style lang="scss">
+$active-color: #007bff;
 .node circle {
-  fill: #fff;
-  stroke: steelblue;
-  stroke-width: 3px;
+	fill: #fff;
+	stroke: $active-color;
+	stroke-width: 2px;
+
+	&.has-children{
+		fill: lighten($active-color, 40%);
+	}
+}
+
+circle.node{
+	fill: #fff;
 }
 
 .node text {
-  font: 12px sans-serif;
+	font: 12px sans-serif;
 }
 
 .link {
-  fill: none;
-  stroke: #ccc;
-  stroke-width: 2px;
+	fill: none;
+	stroke: #ccc;
+	stroke-width: 2px;
 }
 </style>
 
